83134462b8fae3140be737eeda98a6d410b473ab
Nathan V Garrett <ngarrett768@gmail.com>
2023-09-18 07:58:17 -0300

    @nathangarret adicionando testes de lógica

--- indexO.js
+++ indexO.js
@@ -1,3 +1,5 @@
+/* Olá mundo! */
+
 /* Ler o valor correspondente ao salário mensal (variável SM) de um trabalhador e também o valor do 
 percentual de reajuste (variável PR) a ser atribuído. Apresentar o valor do novo salário (variável NS). */ 
 
